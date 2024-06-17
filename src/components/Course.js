3955bbef1ab6e8d47498c22f59d053213d3c683a
Stephany Milhomem <stephanymilhomem25@gmail.com>
2024-06-17 13:08:39 -0300

    Course summary improvements

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 import './Course.css';
 
 const Course = () => {
+  const [activeModule, setActiveModule] = useState(null);
+
   const courseData = {
+    title: 'Sumário', // Adicione o título do curso
     description: 'Domine o React.js e construa aplicações web interativas com este curso completo, ideal para iniciantes. Abordamos desde os conceitos básicos até a criação de projetos reais.',
-    videoUrl: 'https://youtu.be/FXqX7oof0I4?si=OY4OY6D3XZvnmpdR', 
+    videoUrl: 'https://youtu.be/FXqX7oof0I4?si=OY4OY6D3XZvnmpdR',
     modules: [
-      { 
-        title: 'Introdução ao React', 
+      {
+        title: 'Introdução ao React',
+        progress: '0/6',
+        duration: ' 72 min',
         lessons: [
-          'Instalando React e ferramentas essenciais',
-          'JSX: Sintaxe do React',
-          'Criando seu primeiro componente React', 
-          'Props: Passando dados entre componentes',
-          'Inserindo CSS no React',
-        ] 
+            'Introdução',
+            'Instalando React e ferramentas essenciais',
+            'JSX: Sintaxe do React',
+            'Criando seu primeiro componente React',
+            'Props: Passando dados entre componentes',
+            'Inserindo CSS no React',
+        ],
       },
-    ]
+      {
+        title: 'Componentes e Estado',
+        progress: '0/4',
+        duration: '48 min',
+        lessons: [
+            'Componentes Funcionais e de Classe',
+            'Gerenciando o Estado com useState',
+            'Passando Props entre Componentes',
+            'Criando Componentes Reutilizáveis',
+        ],
+      },
+      {
+        title: 'Eventos e Hooks',
+        progress: '0/3',
+        duration: '20 min',
+        lessons: [
+            'Gerenciando Eventos', 
+            'Usando useEffect', 
+            'Criando Custom Hooks'],
+      },
+    ],
+  };
+
+  const toggleModule = (index) => {
+    setActiveModule(index === activeModule ? null : index);
   };
 
   return (
@@ -33,17 +63,34 @@ const Course = () => {
         </div>
         <div className="col-md-4">
           <div className="course-info p-4 bg-light rounded">
-            <h2>{courseData.title}</h2>
+            <h2>{courseData.title}</h2> {/* Título do curso */}
             <div className="modules-list mt-3">
-              <h3>Sumário</h3>
-              <div className="modules-container"> {/* Adicione um container para os módulos */}
+             {/* <h3>Sumário</h3> {/* Título da lista de módulos */}
+              <div className="modules-container">
                 <ul>
-                  {courseData.modules.map((module, moduleIndex) => (
-                    <li key={moduleIndex} className="module-item">
-                      <button className="module-button">{module.title}</button>
-                      <ul className="buttons-container">
+                  {courseData.modules.map((module, index) => (
+                    <li
+                      key={index}
+                      className={`module-item ${
+                        activeModule === index ? 'active' : ''
+                      }`}
+                    >
+                      <button
+                        className="module-button"
+                        onClick={() => toggleModule(index)}
+                      >
+                        {module.title}
+                        <span className="progress-indicator">
+                          {module.progress}
+                        </span>
+                        <span className="duration">{module.duration}</span>
+                        <span className="arrow-down">▼</span>
+                      </button>
+                      <ul className="lessons-list">
                         {module.lessons.map((lesson, lessonIndex) => (
-                          <li key={lessonIndex}><button className="lesson-button">{lesson}</button></li>
+                          <li key={lessonIndex} className="lesson-item">
+                            {lesson}
+                          </li>
                         ))}
                       </ul>
                     </li>
