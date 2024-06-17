ebd90f5290bf33f8c4173b7c06b82aacf2c71ae3
Stephany Milhomem <stephanymilhomem25@gmail.com>
2024-06-17 13:10:48 -0300

    Value correction in minutes

--- src/components/Course.js
+++ src/components/Course.js
@@ -37,7 +37,7 @@ const Course = () => {
       {
         title: 'Eventos e Hooks',
         progress: '0/3',
-        duration: '20 min',
+        duration: '40 min',
         lessons: [
             'Gerenciando Eventos', 
             'Usando useEffect', 
