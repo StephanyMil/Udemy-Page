c76cd02763f0d74ffc284cd089587d760b882cf5
Stephany Milhomem <stephanymilhomem25@gmail.com>
2024-06-18 09:03:13 -0300

    Correction for better practices

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-dark bg-custom">
-        <a className="navbar-brand" href="#">
-          <img src="./images/udemy.png" alt="Udemy Logo" />
-        </a>
-        <h1 className="course-title">React.js Completo: Do Iniciante ao Profissional</h1> {/* Título do curso dentro da navbar */}
+      <a className="navbar-brand" href="/">
+        <img src="./images/udemy.png" alt="Udemy Logo" />
+      </a>
+        <h1 className="course-title"> | React.js Completo: Do Iniciante ao Profissional</h1> {/* Título do curso dentro da navbar */}
       </nav>
       <main className="main-content">
         <Course />
